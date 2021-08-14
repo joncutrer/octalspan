dbe3c8ee54af6adce162d7950d51b86fe54dc712
JC <joncutrer@gmail.com>
2021-08-13 19:57:47 -0500

    working on installer

--- build-run.bat
+++ build-run.bat
@@ -1,9 +1,11 @@
 @echo off
+
+go-winres make
 echo building bin/octalspan.syslogd.exe
 go build -o bin/octalspan.syslogd.exe
 echo copy config to bin/octalspan.yml 
 cp --f octalspan-default.yml bin/octalspan.yml 
-
+cp --f LICENSE bin/LICENSE 
 echo build complete
 
 cd bin
--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 @echo off
+
+go-winres make
 echo building bin/octalspan.syslogd.exe
 go build -o bin/octalspan.syslogd.exe
 echo copy config to bin/octalspan.yml 
 cp --f octalspan-default.yml bin/octalspan.yml 
-
+cp --f LICENSE bin/LICENSE 
 echo build complete
